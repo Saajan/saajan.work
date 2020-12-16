e5dab2e6a3ae052235277d08796282d74671a0b7
Saajan <saajan.sn@gmail.com>
2020-12-16 20:16:33 +0530

    update meta info

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,11 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name=viewport content="width=device-width, initial-scale=1">
           <meta name="theme-color" content="#000000" />
           <meta name="keywords" content="saajan,sn,sullia,fullstack,javasccript,react,kvg,pindibana"></meta>
-          <meta name="Description" content="Saajan SN is the fullstack developer specialized in Javascript Eco system. Has working experience of around 7 years in the Web application development.
+          <meta name="description" content="Saajan SN is the fullstack developer specialized in Javascript Eco system. Has working experience of around 7 years in the Web application development.
           Currently working for Conviva. Saajan is also into Music production and into DJing in free time. Has MSc Tech degree from Manipal University annd also BE degree from KVG College of engineering, Sullia "></meta>
           <link
             href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"
