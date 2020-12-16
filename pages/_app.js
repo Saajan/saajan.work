0c364db90301bfec4d70ba7cf25c6ca85ed6af0a
Saajan <saajan.sn@gmail.com>
2020-12-16 23:43:54 +0530

    title change

--- pages/_app.js
+++ pages/_app.js
@@ -48,7 +48,7 @@ export default class MyApp extends App {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <title>Saajan Work</title>
+          <title>Saajan SN Personel Website</title>
         </Head>
         <Layout>
           <AnimatePresence exitBeforeEnter>
